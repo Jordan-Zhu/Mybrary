45ee173fa893e072f911f086c6836b78bbeae324
Jordan Zhu <zhujordanzhu@gmail.com>
2020-08-04 18:28:54 -0400

    Google Adsense

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose.connect(process.env.DATABASE_URL, {
 useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
-db.once('open', () => console.error('Connected to Mongoose'))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 // root
 app.use('/', indexRouter)
